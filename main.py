68ad84ecd11e021a6b06f6d6b5f6d203a98c265b
Andres U Lead <andres.uribe@ulead.ac.cr>
2025-02-17 19:00:39 -0600

    testing

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+users = []
+
+class User(BaseModel):
+    name: str
+    email: str
+
+@app.post("/users/")
+def create_user(user: User):
+    users.append(user)
+    return {"message": "User created", "user": user}
+
+@app.get("/users/")
+def get_users():
+    return users
+
